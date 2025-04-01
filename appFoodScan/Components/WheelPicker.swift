267e24a5b22049cce7d13c632107e1b1a780f865
CharlieLemonade <carloslopzt15@gmail.com>
2025-03-31 23:48:34 -0600

    Cameraview

---
+++ appFoodScan/Components/WheelPicker.swift
@@ -0,0 +1,27 @@
+//
+//  WheelPicker.swift
+//  appFoodScan
+//
+//  Created by Carlos López on 30/03/25.
+//
+
+import SwiftUI
+
+struct WheelPicker: View {
+    var body: some View {
+        
+        GeometryReader{
+            let size = $0.size
+        }
+        
+        /*struct Config: Equatable {
+            var count: Int
+            var steps: Int = 10
+            var spacing: CGFloat = 5
+        }*/
+    }
+}
+
+#Preview {
+    WheelPicker()
+}
