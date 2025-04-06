2f95ea89ba34970a7f7affe69bd15ad155f88369
CharlieLemonade <carloslopzt15@gmail.com>
2025-04-06 15:54:00 -0600

    Update .gitignore to exclude GoogleService-Info.plist

---
+++ appFoodScan/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  appFoodScan
+//
+//  Created by Carlos López on 06/04/25.
+//
+
+import Foundation
---
+++ appFoodScan/Core/Authentication/ViewModels/RegistrationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewModel.swift
+//  appFoodScan
+//
+//  Created by Carlos López on 06/04/25.
+//
+
+import Foundation
