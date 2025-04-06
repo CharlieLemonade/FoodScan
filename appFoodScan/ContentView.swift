e711a70c46f10b12865e6a87be24ec8500db354e
CharlieLemonade <carloslopzt15@gmail.com>
2025-04-05 21:16:35 -0600

    Font Update

--- appFoodScan/ContentView.swift
+++ appFoodScan/ContentView.swift
@@ -9,18 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    init() {
-        for familyName in UIFont.familyNames {
-            print(familyName)
-            
-            for fontName in UIFont.fontNames(forFamilyName: familyName) {
-                print("-- \(fontName) ")
-            }
-        }
-    }
-    
     var body: some View {
-        Text("dd")
+        SplashScreen()
     }
 }
 
